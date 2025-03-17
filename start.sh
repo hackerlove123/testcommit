1ecf7df9a5a0ba9af2ff4e3afa0437c580cf8b8b
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-03-17 07:02:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -75,9 +75,9 @@ PRXSCAN_PID=$!
 ./monitor.sh &
 MONITOR_PID=$!
 
-# Đợi 9 phút 59 giây (599 giây)
-echo "Đang đợi 9 phút 50 giây..."
-countdown 590 &
+# Đợi 9 phút 585 giây (585 giây)
+echo "Đang đợi 9 phút 585 giây..."
+countdown 585 &
 COUNTDOWN_PID=$!
 
 # Đợi countdown hoàn thành
